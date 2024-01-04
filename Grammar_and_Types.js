e622bade03eed263ee6f32c923723ad0f0adde07
天使不吃糖 <142178737+NoSugars@users.noreply.github.com>
2024-01-03 21:46:16 -0600

    Add files via upload

---
+++ Grammar_and_Types.js
@@ -0,0 +1,98 @@
+
+const p=(s)=>console.log(s);
+
+
+
+// {
+// 	// var globalvar;
+// 	const fruit = 2;
+// 	let Fruit = 3;
+// 	{
+// 		const fruit = 'f';
+// 		let Fruit = 'F';
+// 	}
+// 	p(fruit);
+// 	p(Fruit);
+
+// }
+
+
+
+
+
+
+// {
+// 	const _aaa=4;
+// 	const $aaa=5;
+// 	const bbb$=8;
+
+// 	p(_aaa)
+// 	p($aaa)
+// 	p(bbb$)
+// }
+
+
+
+// {
+// 	const obj={
+// 		name:'name1',
+// 		age:22,
+// 		go(){p('gogogo')}
+// 	}
+// 	const {w} = obj;
+// 	const {age} = obj;
+// 	const {go} = obj;
+
+// 	{
+// 		gg=9;
+// 		//p(window.gg)
+// 		//p(globalThis.gg)
+// 	}
+
+// 	go()
+// 	p(w)
+// 	p(age)
+// 	p(go)
+// 	go()
+// }
+
+
+
+// what is symbol
+
+// {
+// 	const i=parseInt('213af')
+// 	const f=parseFloat('32.213df')
+
+// 	p(i)
+// 	p(f)
+
+// 	const x = parseInt('11',2)
+// 	const y = parseInt('FF',16)
+// 	p(x)
+// 	p(y)
+
+// 	const a='1.1'
+// 	p(a+a)
+// 	p(a+2)
+// 	p(2+a)
+// 	p(+a+2)
+// 	p((+a)+(+a))
+
+// }
+
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
